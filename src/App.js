81a6236709a1f01fe841a64be8bd291817069e15
Bjørnar Kjenaas Mælum <bjornarmaelum@gmail.com>
2021-04-09 21:34:18 +0200

    Understanding State and Props

--- src/App.js
+++ src/App.js
@@ -5,18 +5,64 @@ import Item from "./item.js";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-  
-    <ol>
-      <Item done={false} text={"First Todo"} /> 
-    </ol>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newItemValue: "",
+      items: [
+        {text : "First Todo", done: false },
+        {text : "Second Todo", done: false },
+        {text : "Third Todo", done: true },
+        {text : "Fourth Todo", done: true },
 
+      ]
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
-    </div>
-  );
+  }
+
+  handleChange(event) {
+    this.setState({ newItemValue: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let items = this.state.items.slice()
+
+    items.push({
+      text: this.state.newItemValue,
+      done: false,
+    })
+
+    this.setState({
+      newItemValue: '',
+      items: items
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newItemValue} placeholder="New todo..." onChange={this.handleChange} />
+          <input type="submit" value="Add Item" />
+        </form>
+    
+      <ol>
+        {this.state.items.map((item) => (
+          <Item key={item.id} done={item.done} text={item.text} />
+      ))}
+      </ol>
+      </div>
+    );
+
+  }
 }
 
-export default App;
+
+export default App; 
